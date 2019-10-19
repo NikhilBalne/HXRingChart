8e0a5b1562cd05fbbb9a932b9cce8b90666dae56
Nikhil Balne <nikhilkumar.balne@gmail.com>
2019-10-19 16:05:51 +0530

    Initial Commit

---
+++ HXChart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXChart
+//
+//  Created by iHub on 19/10/19.
+//  Copyright © 2019 iHubTechnologiesPvtLtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXChart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXChart
+//
+//  Created by iHub on 19/10/19.
+//  Copyright © 2019 iHubTechnologiesPvtLtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
